ead3047a31078b9e9cb61ef24e54a90f20df2407
Karpov Yan <oficialbalunov@gmail.com>
2020-12-21 22:53:52 +0800

    Day 1 is done

--- scripts/index.js
+++ scripts/index.js
@@ -10,21 +10,15 @@ const playerBtn = document.querySelectorAll('.player-btn'),
 
 const deactivationPlayer = () => {
     temp.style.display = 'none';
-    playerBtn.forEach((item) => {
-        item.classList.remove('active');
-    });
-    playerBlock.forEach((item) => {
-        item.classList.remove('active');
-    });
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
 };
 
-playerBtn.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
         deactivationPlayer();
         btn.classList.add('active');
         playerBlock[i].classList.add('active');
-    });
-});
+}));
 
 radioPlayerInit();
 musicPlayerInit();
